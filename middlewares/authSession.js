98942be0689df304f99d93e5b7405e24f6d0ba7a
bruno7even <brunob2013@hotmail.com.ar>
2023-11-11 20:52:04 -0300

    express-session

--- middlewares/authSession.js
+++ middlewares/authSession.js
@@ -1,16 +1,12 @@
 const db = require('../database/models');
 
-// RETORNA AL HOME Y NO PERMITE INGRESAR AL FORM DE LOGIN Y REGISTER EN CASO DE ESTAR VERIFICADO
-
 const authRedirectSession = (req, res, next) => {
-    if (req.session && req.session.userId) { // requerimos si hay una sesion registrada
+    if (req.session && req.session.userId) { 
     
-        return res.redirect('/'); // si la hay redirecciona para evitar que ingrese al login/register
+        return res.redirect('/');
     }
     next(); 
 };
-
-// VERIFICAMOS LA SESSION, EN CASO DE QUE NO ESTE LOGUEADO, REDIRECCIONA AL LOGIN
 
 const authSession = (req, res, next) => {
     if (req.session && req.session.userId) { 
