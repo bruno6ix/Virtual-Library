9bb901e57e7b421785edcf7c378c528009790503
bruno7even <brunob2013@hotmail.com.ar>
2023-11-12 03:43:22 -0300

    nube

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,11 @@
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require("../../config");
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": '123456789',
-    "database": "biblioteca_db",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
     "dialect": "mysql"
   },
   "test": {
