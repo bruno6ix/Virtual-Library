ce17a693deeb307d39050599d07a81202bb048ca
bruno7even <brunob2013@hotmail.com.ar>
2023-11-12 03:57:34 -0300

    nube 2

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-export const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '123456789'
-export const DB_NAME = process.env.DB_NAME || 'testdb'
-export const DB_PORT = process.env.DB_PORT || 3306
-// jwt secret key
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || '123456789'
+const DB_NAME = process.env.DB_NAME || 'testdb'
+const DB_PORT = process.env.DB_PORT || 3306
+
+module.exports = {
+    PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+}
+
