e02e112bddf91552d95c4d07f9503b56b959df20
bruno7even <brunob2013@hotmail.com.ar>
2023-11-12 03:59:54 -0300

    nube 3

--- database/config/config.js
+++ database/config/config.js
@@ -9,17 +9,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
     "dialect": "mysql"
   }
 }
