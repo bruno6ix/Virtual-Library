525cf68958b59275fbca2fc6b5cdc7b2b02b4048
bruno7even <brunob2013@hotmail.com.ar>
2023-11-14 18:10:56 -0300

    prueba

--- database/config/config.js
+++ database/config/config.js
@@ -7,7 +7,6 @@ const sequelize = new Sequelize({
   dialect: "mysql",
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
-  use_env_variable: '?',
 });
 
 module.exports = {
@@ -19,6 +18,5 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
-    use_env_variable: '?',
   },
 };
