56be95c299d13ddaab54836169393112e7657fb5
bruno7even <brunob2013@hotmail.com.ar>
2023-11-14 19:06:37 -0300

    prueba 7

--- database/models/index.js
+++ database/models/index.js
@@ -5,10 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
-const env = process.env.NODE_ENV || 'development';
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
