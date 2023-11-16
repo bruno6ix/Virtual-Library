aacd6952fd7a2169e362978e4bb0d6b97de14261
bruno7even <brunob2013@hotmail.com.ar>
2023-11-15 21:55:28 -0300

    merge

--- database/config/config.js
+++ database/config/config.js
@@ -1,26 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  "development": {
-    "username": 'brunito',
-    "password": '',
-    "database": 'biblioteca_db',
-    "host": '127.0.0.1',
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": 'brunito',
-    "password": '',
-    "database": 'biblioteca_db',
-    "host": '127.0.0.1',
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": 'brunito',
-    "password": '',
-    "database": 'biblioteca_db',
-    "host": '127.0.0.1',
-    "dialect": "mysql"
-=======
   development: {
     username: 'root',
     password: '123456789',
@@ -41,12 +19,5 @@ module.exports = {
     database: 'biblioteca_db',
     host: '127.0.0.1',
     dialect: 'mysql',
-<<<<<<< HEAD
-=======
-    dialectOptions: {
-      connectTimeout: 60000,
-    },
->>>>>>> 3e135a74f9f9fc33abff13590f6f1ac8de5116a0
->>>>>>> 165d33c5776f6cd1824400d0caa22cb3a4ecb035
   }
 };
