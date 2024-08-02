6fc63bd0e4d5c95aebf23c3097df610a37b3c43f
bruno7even <brunob2013@hotmail.com.ar>
2024-08-02 18:09:44 -0300

    fwdfsd

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,25 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'GUzKNljaODMGAKTEwfYgdiTraHPOjveb',
+    password: 'PhcKnFsXJCVftgTiWJomNjHnpPntwZYv',
     database: 'railway',
-    host: 'mysql-tfho.railway.internal',
+    host: 'roundhouse.proxy.rlwy.net',
     port: '20846',
     dialect: 'mysql',
   },
   test: {
     username: 'root',
-    password: 'GUzKNljaODMGAKTEwfYgdiTraHPOjveb',
+    password: 'PhcKnFsXJCVftgTiWJomNjHnpPntwZYv',
     database: 'railway',
-    host: 'mysql-tfho.railway.internal',
+    host: 'roundhouse.proxy.rlwy.net',
     port: '20846',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
-    password: 'GUzKNljaODMGAKTEwfYgdiTraHPOjveb',
+    password: 'PhcKnFsXJCVftgTiWJomNjHnpPntwZYv',
     database: 'railway',
-    host: 'mysql-tfho.railway.internal',
+    host: 'roundhouse.proxy.rlwy.net',
     port: '20846',
     dialect: 'mysql',
   }
