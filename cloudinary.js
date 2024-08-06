2ee33893e6d455f44600409f6628144328e15a50
Bruno B. <77407109+bruno6ix@users.noreply.github.com>
2024-08-06 12:09:00 -0300

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name:'dgz9c8zhv',
-    api_key:'528525288253361',
-    api_secret:'hrDpbrlN274uo562gUn67AmFCxw'
+    cloud_name:'',
+    api_key:'',
+    api_secret:''
 })
 
 module.exports = cloudinary;
